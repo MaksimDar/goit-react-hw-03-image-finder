ddee1a3dd2ddd6e2b0d9aaea220537a0229308de
Maksim1903 <dovgusha.m@gmail.com>
2023-02-28 13:31:07 +0100

    .

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export {Searchbar} from './Searchbar';
-export {ImageGallery} from './ImageGallery';
-export {ImageGalleryItem} from './ImageGalleryItem';
-export {Button} from './Button';
-export {Modal} from './Modal';
-export {Loader} from './Loader';
